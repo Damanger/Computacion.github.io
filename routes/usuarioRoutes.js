fe0ff4ed9603759a15dee23d13b9ac6c76cde07f
Damanger <92276635+Damanger@users.noreply.github.com>
2022-12-11 11:30:30 -0600

    Add files via upload

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usuariosRoutes = void 0;
+const express_1 = require("express");
+const usuarioController_1 = require("../controllers/usuarioController");
+class UsuariosRoutes {
+    constructor() {
+        this.router = (0, express_1.Router)();
+        this.config();
+    }
+    config() {
+        this.router.post('/verificarUsuario', usuarioController_1.usuariosController.verificaUsuario);
+        this.router.get('/esActivo/:id', usuarioController_1.usuariosController.esActivo);
+        this.router.get('/', usuarioController_1.usuariosController.list);
+    }
+}
+exports.usuariosRoutes = new UsuariosRoutes();
+exports.default = exports.usuariosRoutes.router;
